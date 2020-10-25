67f393f2434e8fa0e7374458509182536f4d5211
MehrdadHosseinNejad74 <hosseinnejad74@gmail.com>
2020-10-25 21:06:14 +0330

    Some change in size of shots.dart

--- lib/screen/detail_screen/component/shots.dart
+++ lib/screen/detail_screen/component/shots.dart
@@ -13,87 +13,90 @@ class Shots extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SectionTitle(value: 'Travelers’ shots',),
+        SectionTitle(
+          value: 'Travelers’ shots',
+        ),
         SizedBox(
           height: kDefaultPadding / 2,
         ),
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                height: size.height * 0.3,
-                width: size.width * 0.3,
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image:
-                            AssetImage("assets/images/travelershots/los.png")),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-            SizedBox(
-              width: kDefaultPadding / 2,
+            Container(
+              height: size.height * 0.3,
+              width: size.width * 0.3,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/travelershots/los.png")),
+                  borderRadius: BorderRadius.circular(10)),
             ),
             Expanded(
-              flex: 2,
               child: Container(
                 height: size.height * 0.3,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Container(
-                          height: size.width * 0.28,
-                          width: size.width * 0.28,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                      "assets/images/travelershots/san.png")),
-                              borderRadius: BorderRadius.circular(10)),
+                          width: size.width * 0.26,
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              height: size.width * 0.26,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          "assets/images/travelershots/san.png")),
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
                         ),
                         Container(
-                          height: size.width * 0.28,
-                          width: size.width * 0.28,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                      "assets/images/travelershots/sun.png")),
-                              borderRadius: BorderRadius.circular(10)),
+                          width: size.width * 0.26,
+                          child: AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: AssetImage(
+                                          "assets/images/travelershots/sun.png")),
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                          ),
                         ),
                       ],
                     ),
-                    Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          height: size.height * 0.14,
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: AssetImage(
-                                      "assets/images/travelershots/hori.png")),
-                              borderRadius: BorderRadius.circular(10)),
-                        ),
-                        Positioned.fill(
-                          child: DarkGradient(
-                            radius: 10,
+                    Container(
+                      height: size.height * 0.14,
+                      width: size.width * 0.55,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    fit: BoxFit.cover,
+                                    image: AssetImage(
+                                        "assets/images/travelershots/hori.png")),
+                                borderRadius: BorderRadius.circular(10)),
                           ),
-                        ),
-                        Center(
-                            child: Text(
-                          "+ 16 more",
-                          style: kDefaultTextStyle.copyWith(
-                              color: Colors.white,
-                              fontSize: size.width * 0.060),
-                        ))
-                      ],
+                          Positioned.fill(
+                            child: DarkGradient(
+                              radius: 10,
+                            ),
+                          ),
+                          Center(
+                              child: Text(
+                            "+ 16 more",
+                            style: kDefaultTextStyle.copyWith(
+                                color: Colors.white, fontSize: size.width * 0.060),
+                          ))
+                        ],
+                      ),
                     ),
                   ],
                 ),
